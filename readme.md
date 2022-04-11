eab1ec45ca4bbc4cfe62036bea5fb3243b447968
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-11 17:58:11 +0800

    improve performance using worker thread

--- readme.md
+++ readme.md
@@ -1,2 +1,7 @@
 # performance tuning in Node
 
+## real life block example
+    JSON.stringify()
+    JSON.parse()
+    [5,1,2,3,4].sort()
+    security related functions -> crypto.pbkdf2
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cluster = require('cluster')
 
 const app = express()
 
@@ -11,15 +12,35 @@ function delay(duration) {
 }
 
 app.get('/', (req, res) => {
-    res.send('performance example')
+    res.send(`performance example ${process.pid}`) // different worker has different pid
 })
 
 app.get('/timer', (req, res) => {
     // delay the response on purpose
     delay(5000)  // the server cannot process any other request, even like query DB
-    res.send('Ding ding ding...')
+    res.send(`Ding ding ding... ${process.pid}`)
 })
 
-app.listen(3000, () => {
-    console.log('server is listening at port 3000');
-})
+
+/*
+the output will be when you run npm start:
+    running server process
+    Master has been started
+    running server process
+    worker process started
+    running server process
+    worker process started
+*/
+console.log('running server process');
+if (cluster.isPrimary) {
+    console.log('Master has been started');
+    cluster.fork()
+    cluster.fork()  // fork two workers
+} else {
+    console.log('worker process started');
+    app.listen(3000) // each worker listen on the same port
+}
+
+// app.listen(3000, () => {
+//     console.log('server is listening at port 3000');
+// })
